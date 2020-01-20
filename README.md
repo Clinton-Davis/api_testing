fd634cc263392f150be2c65337230edb5b35a32c
Clinton Davis <you@example.com>
2020-01-20 15:03:17 +0000

    got google maps to show

---
+++ README.md
@@ -0,0 +1,1 @@
+Weather Key 93fc6b46edd4b57ed1e18af04d5ead0c
--- weather_api.js
+++ weather_api.js
@@ -1,1 +1,8 @@
-console.log("hello");
+const url =
+	"http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID93fc6b46edd4b57ed1e18af04d5ead0capi.openweathermap.org/data/2.5/weather?q=London,uk";
+
+function getinput() {
+	fetch(url).then(function(responce) {
+		console.log(responce);
+	});
+}
