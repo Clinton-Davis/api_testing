9810b87a46ab87b38eb50e0359715c3e1c5f91b3
Clinton Davis <you@example.com>
2020-01-22 13:43:28 +0000

    got api working and on cape town

--- weather_api.js
+++ weather_api.js
@@ -1,11 +1,10 @@
 var url =
-	"https://api.openweathermap.org/data/2.5/weather?q=dublin&appid=93fc6b46edd4b57ed1e18af04d5ead0c";
+	"https://api.openweathermap.org/data/2.5/weather?id=1085599&units=metric&APPID=93fc6b46edd4b57ed1e18af04d5ead0c";
 var output = document.querySelector("#output");
 var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function() {
 	if (xhr.readyState === 4 && xhr.status === 200) {
-		var str = xhr.responseText;
-		var obj = JSON.parse(str);
+		var obj = JSON.parse(xhr.responseText);
 		output.innerHTML = obj.main;
 		console.log(obj);
 	}
@@ -13,17 +12,3 @@ xhr.onreadystatechange = function() {
 xhr.open("GET", url);
 xhr.send();
 console.log(xhr);
-
-/*function getinput() {
-	const xhr = new XMLHttpRequest();
-
-	xhr.onload = function() {
-		if (this.readyState == 4 && this.status == "200") {
-			let data = JSON.parse(xhr.responseText).results;
-			outputHTML(data);
-		}
-	};
-	xhr.open("GET", tempURL);
-	xhr.send();
-	console.log(intake.value);
-}*/
