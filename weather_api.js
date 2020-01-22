82dd63ed0df3364f05a9156d22763fce449de4d0
Clinton Davis <you@example.com>
2020-01-22 15:49:19 +0000

    Getting data to console need to get it to doc

--- weather_api.js
+++ weather_api.js
@@ -1,14 +1,22 @@
-var url =
-	"https://api.openweathermap.org/data/2.5/weather?id=1085599&units=metric&APPID=93fc6b46edd4b57ed1e18af04d5ead0c";
-var output = document.querySelector("#output");
-var xhr = new XMLHttpRequest();
+const url = "https://api.openweathermap.org/data/2.5/weather?id=1085599";
+
+const my_id = "&APPID=93fc6b46edd4b57ed1e18af04d5ead0c";
+var unit = "&units=metric";
+var unit = "&units=metric";
+const xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function() {
-	if (xhr.readyState === 4 && xhr.status === 200) {
-		var obj = JSON.parse(xhr.responseText);
-		output.innerHTML = obj.main;
-		console.log(obj);
+	if (this.readyState === 4 && this.status === 200) {
+		var obj = JSON.parse(this.responseText);
+
+		var city = obj.name;
+		var temps = "H " + obj.main.temp_max + " / " + obj.main.temp_min + " L";
+		console.log(city);
+		console.log(temps);
+		writeToDoc();
+	}
+	function writeToDoc() {
+		document.getElementById("#weather").innerHTML = obj + "<br>" + temps;
 	}
 };
-xhr.open("GET", url);
+xhr.open("GET", url + my_id + unit);
 xhr.send();
-console.log(xhr);
